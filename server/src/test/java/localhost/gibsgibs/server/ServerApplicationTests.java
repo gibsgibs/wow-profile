4976fc32a1033fddf8bbe9d3ec1b0e85e693d06f
gibsgibs-desktop <g.brehm@hotmail.com>
2023-10-24 13:29:34 -0700

    switched to java spring backend

---
+++ server/src/test/java/localhost/gibsgibs/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package localhost.gibsgibs.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
